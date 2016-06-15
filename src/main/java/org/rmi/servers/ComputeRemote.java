f216246110e2537f67036f285c0a26a046054feb
Thammachart <bustheheroall@gmail.com>
2016-06-15 20:40:10 +0700

    Server Complete!

---
+++ src/main/java/org/rmi/servers/ComputeRemote.java
@@ -0,0 +1,37 @@
+package org.rmi.servers;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+import org.rmi.components.ComputeRemoteInterface;
+import org.rmi.components.Task;
+
+public class ComputeRemote implements ComputeRemoteInterface {
+
+    public <T> T executeTask(Task<T> task) {
+        return task.execute();
+    }
+
+    public static void main(String args[]) {
+        if(System.getSecurityManager() == null) {
+            System.setSecurityManager(new SecurityManager());
+        }
+
+        try {
+            String name = "Compute";
+            ComputeRemoteInterface remote = new ComputeRemote();
+            ComputeRemoteInterface stub = (ComputeRemoteInterface) UnicastRemoteObject.exportObject(remote, 0);
+
+            System.out.println("Locating Registry...");
+            Registry registry = LocateRegistry.getRegistry();
+            registry.rebind(name, stub);
+            System.out.println("ComputeRemote bound");
+        } catch(Exception e) {
+            System.err.println("Exception: ");
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+}
